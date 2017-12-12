4cb2c7ff5cbbc28c4136ba3d6dc6b12924c4df85
tokada <ubiqnet@gmail.com>
2017-12-12 11:16:29 +0900

    refactor and add env check

--- index.js
+++ index.js
@@ -12,66 +12,77 @@ var request = require('request');
 var ejs = require('ejs');
 var fs = require('fs');
 
+var makeResponse = function(statusCode, body) {
+  if (typeof(body) != 'string') {
+    body = JSON.stringify(body);
+  }
+  return {
+    statusCode: statusCode,
+    headers: { 'Content-Type': 'application/json' },
+    body: body
+  };
+}
+
+var chooseTemplate = function(templateName) {
+  return fs.readFileSync('templates/'+templateName+'.ejs', 'utf-8');
+}
+
 exports.handler = function(event, context) {
   var requestBody = JSON.parse(event.body);
 
+  if (!process.env.CHATWORK_TOKEN || !process.env.CHATWORK_ROOM_ID) {
+    context.done('error', makeResponse(200, { error: { message: "Environment variable CHATWORK_TOKEN or CHATWORK_ROOM_ID required" } }));
+    return;
+  }
+
   // choose template
   var template = null;
-  if (requestBody.issue != null && requestBody.action == 'opened') {
-    template = fs.readFileSync('templates/open_issue.ejs', 'utf-8');
+  if (requestBody.issue && requestBody.action == 'opened') {
+    template = chooseTemplate('open_issue');
   }
-  else if (requestBody.issue != null && requestBody.action == 'closed') {
-    template = fs.readFileSync('templates/close_issue.ejs', 'utf-8');
+  else if (requestBody.issue && requestBody.action == 'closed') {
+    template = chooseTemplate('close_issue');
   }
-  else if (requestBody.pull_request != null && requestBody.action == 'opened') {
-    template = fs.readFileSync('templates/open_pull_request.ejs', 'utf-8');
+  else if (requestBody.pull_request && requestBody.action == 'opened') {
+    template = chooseTemplate('open_pull_request');
   }
-  else if (requestBody.pull_request != null && requestBody.action == 'closed') {
-    template = fs.readFileSync('templates/close_pull_request.ejs', 'utf-8');
+  else if (requestBody.pull_request && requestBody.action == 'closed') {
+    template = chooseTemplate('close_pull_request');
   }
-  else if (requestBody.issue != null && requestBody.comment != null && requestBody.issue.pull_request != null) {
-    template = fs.readFileSync('templates/comment_pull_request.ejs', 'utf-8');
+  else if (requestBody.issue && requestBody.comment && requestBody.issue.pull_request) {
+    template = chooseTemplate('comment_pull_request');
   }
-  else if (requestBody.issue != null && requestBody.comment != null) {
-    template = fs.readFileSync('templates/comment_issue.ejs', 'utf-8');
+  else if (requestBody.issue && requestBody.comment) {
+    template = chooseTemplate('comment_issue');
   }
-  else if (requestBody.pusher != null && requestBody.before.match(/^0000000/)) {
-    template = fs.readFileSync('templates/create_branch.ejs', 'utf-8');
+  else if (requestBody.pusher && requestBody.before.match(/^0000000/)) {
+    template = chooseTemplate('create_branch');
   }
-  else if (requestBody.pusher != null) {
-    template = fs.readFileSync('templates/push.ejs', 'utf-8');
+  else if (requestBody.pusher) {
+    template = chooseTemplate('push');
   }
 
-  if (template == null) {
-    var response = {
-      statusCode: 200,
-      headers: {},
-      body: JSON.stringify({ error: { message: "no template matched for request body", request_body: event.body } })
-    };
-    context.done('error', response);
+  if (!template) {
+    context.done('error', makeResponse(200, { error: { message: "No template matched for request body", body: event.body } }));
+    return;
   }
-  else {
-    var msg = ejs.render(template, { event: requestBody });
-    var options = {
-      url: 'https://api.chatwork.com/v2/rooms/' + process.env.CHATWORK_ROOM_ID +'/messages',
-      headers: {
-        'X-ChatWorkToken': process.env.CHATWORK_TOKEN
-      },
-      form : { body : msg },
-      useQuerystring: true
-    };
 
-    request.post(options, function(err, res, body) {
-      var response = {
-        statusCode: 200,
-        headers: {},
-        body: body
-      };
-      if (!err && res.statusCode == 200) {
-        context.succeed(response);
-      }else{
-        context.done('error', response);
-      }
-    });
-  }
+  var msg = ejs.render(template, { event: requestBody });
+  var options = {
+    url: 'https://api.chatwork.com/v2/rooms/' + process.env.CHATWORK_ROOM_ID +'/messages',
+    headers: {
+      'X-ChatWorkToken': process.env.CHATWORK_TOKEN
+    },
+    form : { body : msg },
+    useQuerystring: true
+  };
+
+  request.post(options, function(err, res, body) {
+    var response = makeResponse(200, body);
+    if (!err && res.statusCode == 200) {
+      context.succeed(response);
+    }else{
+      context.done('error', response);
+    }
+  });
 };
